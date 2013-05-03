dba3a2cd8fcba1b9f175c20f3c1c964dd2c18d4d
Tony Zhai <tianlong.zhai@gmail.com>
2013-05-03 23:02:28 +0800

    refactory project, change blog to shop

---
+++ domain/src/main/java/com/tonyzhai/shop/portal/goods/domain/support/GoodsCommentCategory.java
@@ -0,0 +1,1 @@
+package com.tonyzhai.shop.portal.goods.domain.support;/** * Goods comment category * * @author Tony Zhai * @since 13-5-3 PM10:31 */public enum GoodsCommentCategory {    GOOD,MIDDLE,BAD}
